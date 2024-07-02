166577b7a94f1b9ececa7176f0e2dcb4f646ff43
yiwwhl <yiwwhl@qq.com>
2024-07-02 16:28:48 +0800

    fix: simplify processing logic and avoid duplicate dependency collection

--- src/core/Processors/Data/index.ts
+++ src/core/Processors/Data/index.ts
@@ -20,6 +20,7 @@ export default class DataProcessor {
   stableSchemas: Ref<StabledSchema[]>; // stableSchmeas from RenderRuntime
   model: Ref<AnyObject>;
   effects: Record<string, Set<AnyFunction>> = {};
+  inputFilter = new Set();
   keysWithEffectsByTargetWithEffects = new Map();
   afterModelUpdateEffects = new Map<AnyObject, Set<AnyFunction>>();
   modelProcessProgress = new Map();
@@ -43,6 +44,7 @@ export default class DataProcessor {
         if (!isEqual(newValue, this.prevModelState.value)) {
           for (const key of Object.keys(newValue)) {
             if (!isEqual(newValue[key], this.prevModelState.value[key])) {
+              console.log("key", key, this.effects);
               if (this.effects[key]) {
                 Array.from(this.effects[key])?.forEach((effect) => {
                   effect();
@@ -120,6 +122,11 @@ export default class DataProcessor {
             this.effects[field] = new Set();
           }
           if (update) {
+            if (this.inputFilter.has(input)) {
+              return;
+            } else {
+              this.inputFilter.add(input);
+            }
             const effect = () => {
               const executionResult = input({ model: this.model.value });
               this.processValueSyncOrAsync({
@@ -131,53 +138,19 @@ export default class DataProcessor {
                 update: (res: any) => {
                   update(res);
                   if (isHandlingDefaultValue) {
-                    if (this.model.value[field] instanceof Promise) {
-                      this.model.value[field].then((fieldValue: any) => {
-                        this.model.value[field] = fieldValue;
-                        const executionResult = input({
-                          model: this.model.value,
-                        });
-                        this.processValueSyncOrAsync({
-                          input: executionResult,
-                          key,
-                          excludeDeepProcessing:
-                            this.nonDeepProcessableKeys.includes(key),
-                          rawUpdate: update,
-                          update: (res: any) => {
-                            update(res);
-                            if (isHandlingDefaultValue) {
-                              let afterModelUpdateEffects =
-                                this.afterModelUpdateEffects.get(target);
-                              if (!afterModelUpdateEffects) {
-                                afterModelUpdateEffects = new Set();
-                              }
-                              afterModelUpdateEffects.add(() => {
-                                this.modelProcessProgress.set(target, true);
-                              });
-                              this.afterModelUpdateEffects.set(
-                                target,
-                                afterModelUpdateEffects
-                              );
-                              this.effects[field].delete(effect);
-                            }
-                          },
-                        });
-                      });
-                    } else {
-                      let afterModelUpdateEffects =
-                        this.afterModelUpdateEffects.get(target);
-                      if (!afterModelUpdateEffects) {
-                        afterModelUpdateEffects = new Set();
-                      }
-                      afterModelUpdateEffects.add(() => {
-                        this.modelProcessProgress.set(target, true);
-                      });
-                      this.afterModelUpdateEffects.set(
-                        target,
-                        afterModelUpdateEffects
-                      );
-                      this.effects[field].delete(effect);
+                    let afterModelUpdateEffects =
+                      this.afterModelUpdateEffects.get(target);
+                    if (!afterModelUpdateEffects) {
+                      afterModelUpdateEffects = new Set();
                     }
+                    afterModelUpdateEffects.add(() => {
+                      this.modelProcessProgress.set(target, true);
+                    });
+                    this.afterModelUpdateEffects.set(
+                      target,
+                      afterModelUpdateEffects
+                    );
+                    this.effects[field].delete(effect);
                   }
                 },
               });
@@ -283,9 +256,7 @@ export default class DataProcessor {
         }
         this.processValueSyncOrAsync({
           input,
-          update: (res: any) => {
-            update(res);
-          },
+          update,
           rawUpdate: update,
           key,
           excludeDeepProcessing: this.nonDeepProcessableKeys.includes(key),
